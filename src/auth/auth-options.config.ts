9a68ec160260ac9ed1a5858300b8d3eb6011abe1
mpcodingpl <97036774+mpcodingpl@users.noreply.github.com>
2024-12-01 14:39:51 +0100

    feat: adds user id to the next auth session object

--- src/auth/auth-options.config.ts
+++ src/auth/auth-options.config.ts
@@ -1,8 +1,23 @@
-import NextAuth from "next-auth"
+import NextAuth, { User, type DefaultSession } from "next-auth"
 import Resend from "next-auth/providers/resend"
+import { JWT } from "next-auth/jwt"
 
 import { prisma } from "@/prisma"
 import { PrismaAdapter } from "@auth/prisma-adapter"
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string
+    } & DefaultSession["user"]
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string
+  }
+}
 
 export const { auth, handlers, signIn, signOut } = NextAuth({
   session: {
@@ -18,5 +33,19 @@ export const { auth, handlers, signIn, signOut } = NextAuth({
   pages: {
     signIn: "/login",
     signOut: "/sign-out",
+  },
+  callbacks: {
+    jwt({ token, user }: { token: JWT; user: User }) {
+      if (user && user.id) {
+        token.id = user.id
+      }
+      return token
+    },
+    session({ session, token }) {
+      if (token.id) {
+        session.user.id = token.id
+      }
+      return session
+    },
   },
 })
