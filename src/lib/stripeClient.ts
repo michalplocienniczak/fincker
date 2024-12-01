b51f9d1f802b90bfe04d543f8eebcf24b4d913cd
mpcodingpl <97036774+mpcodingpl@users.noreply.github.com>
2024-12-01 14:50:51 +0100

    feat: adds and initiates stripe to the application

---
+++ src/lib/stripeClient.ts
@@ -0,0 +1,2 @@
+import { loadStripe } from "@stripe/stripe-js"
+export const stripeClient = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!)
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,26 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatValueWithCurrency = (value: string | number, currencyCode: string) => {
+  try {
+    const formatter = new Intl.NumberFormat("en", {
+      style: "currency",
+      currency: currencyCode,
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 0,
+    })
+
+    const parts = formatter.formatToParts(1)
+    const symbol = parts.find((part) => part.type === "currency")?.value
+
+    const currencySymbol = symbol === "PLN" ? "zł" : symbol
+
+    const currency = symbol === "PLN" ? `${value}${currencySymbol}` : `${currencySymbol}${value}`
+
+    return currency || currencyCode
+  } catch (error) {
+    console.error("Invalid currency code:", currencyCode, error)
+    return currencyCode
+  }
+}
